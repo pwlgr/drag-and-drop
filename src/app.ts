b211388809c3d8542c20f0f9cb00f4a14008816a
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-04-10 17:33:12 +0200

    gather form inputs and validate

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,32 @@
+interface Validatable {
+	value: string | number;
+	required: boolean;
+	minLength?: number;
+	maxLength?: number;
+	min?: number;
+	max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+	let isValid = true;
+	if (validatableInput.required) {
+		isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+	}
+	if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
+		isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+	}
+	if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+		isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+	}
+	if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+		isValid = isValid && validatableInput.value >= validatableInput.min;
+	}
+	if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+		isValid = isValid && validatableInput.value <= validatableInput.max;
+	}
+	return isValid;
+}
+
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
 	const originalMethod = descriptor.value;
 	const adjDescriptor: PropertyDescriptor = {
@@ -34,10 +63,50 @@ class ProjectInput {
 		this.attach();
 	}
 
+	private gatherUserInput(): [string, string, number] | void {
+		const enteredTitle = this.titleInputElement.value;
+		const eneredDescription = this.descriptionInputElement.value;
+		const enteredPeople = this.peopleInputElement.value;
+
+		const titleValidator: Validatable = {
+			value: enteredTitle,
+			required: true
+		};
+		const descriptionValidator: Validatable = {
+			value: eneredDescription,
+			required: true,
+			minLength: 5
+		};
+		const peopleValidator: Validatable = {
+			value: enteredPeople,
+			required: true,
+			min: 1,
+			max: 5
+		};
+
+		if (!validate(titleValidator) || !validate(descriptionValidator) || !validate(peopleValidator)) {
+			alert('Invalid input.');
+			return;
+		} else {
+			return [ enteredTitle, eneredDescription, +enteredPeople ];
+		}
+	}
+
+	private clearInputs() {
+		this.titleInputElement.value = '';
+		this.descriptionInputElement.value = '';
+		this.peopleInputElement.value = '';
+	}
+
 	@autobind
 	private submitHandler(event: Event) {
 		event.preventDefault();
-		console.log(this.titleInputElement.value);
+		const userInput = this.gatherUserInput();
+		if (Array.isArray(userInput)) {
+			const [ title, desc, people ] = userInput;
+			console.log(title, desc, people);
+			this.clearInputs();
+		}
 	}
 
 	private configure() {
